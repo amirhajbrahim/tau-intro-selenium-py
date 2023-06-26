fe5810b9f83746a935d7783641add1092fc10377
amirhajbrahim <amirhadjibrahim@gmail.com>
2023-06-26 16:52:37 +0100

    added search by clicking the button instead of typing RETURN

--- pages/search.py
+++ pages/search.py
@@ -3,7 +3,6 @@ This module contains DuckDuckGoSearchPage,
 the page object for the DuckDuckGo search page.
 """
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 class DuckDuckGoSearchPage:
 
@@ -11,6 +10,7 @@ class DuckDuckGoSearchPage:
     URL = "https://duckduckgo.com/"
     # Locators
     SEARCH_INPUT = (By.ID, 'searchbox_input')
+    SEARCH_BUTTON = (By.XPATH, "//div[@class='searchbox_iconWrapper__suWUe']/button[1]")
 
     # Initializer
     def __init__(self, browser):
@@ -22,4 +22,6 @@ class DuckDuckGoSearchPage:
 
     def search(self, phrase):
         search_input = self.browser.find_element(*self.SEARCH_INPUT)
-        search_input.send_keys(phrase + Keys.RETURN)
+        search_button = self.browser.find_element(*self.SEARCH_BUTTON)
+        search_input.send_keys(phrase)
+        search_button.click()
